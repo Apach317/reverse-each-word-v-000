b98fcee4e75e9871629969bee934f167c66a0137
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-03 00:48:18 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence1)
   new_sentence1 = sentence1.split
   me_hoy = []
-  new_sentence1.map { |sentence1| me_hoy << sentence1.reverse  }
+  new_sentence1.collect { |sentence1| me_hoy << sentence1.reverse  }
   me_hoy.join(" ")
 
 
